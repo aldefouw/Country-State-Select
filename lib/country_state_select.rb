4f8e9295de6cd04b23b5365b43620184258ab385
SVT390 <Adam@Mac-Pro-Adam.local>
2015-10-24 11:34:12 -0500

    Adjustments to suit the Code Climate style guide

--- lib/country_state_select.rb
+++ lib/country_state_select.rb
@@ -1,36 +1,37 @@
-require "country_state_select/version"
-require "city-state"
-require "rails"
-require "sass-rails"
-require "compass-rails"
-require "chosen-rails"
-require "simple_form"
+require 'country_state_select/version'
+require 'city-state'
+require 'rails'
+require 'sass-rails'
+require 'compass-rails'
+require 'chosen-rails'
+require 'simple_form'
 
+# @author : Adam De Fouw
 module CountryStateSelect
 
-  #Collect the Countries
+  # Collect the Countries
   def self.countries_collection
-    CS.countries.except!(:COUNTRY_ISO_CODE).collect {|p| [ p[ 1], p[0] ] }.compact
+    CS.countries.except!(:COUNTRY_ISO_CODE).collect {|p| [p[ 1],p[0]] }.compact
   end
 
-  #Pass array of unwanted countries to get back all except those in the array
+  # Pass array of unwanted countries to get back all except those in the array
   def self.countries_except(*except)
     countries_collection.collect { |c| c unless except.include?(c.second) }.compact
   end
 
-  #Return either the State (String) or States (Array)
+  # Return either the State (String) or States (Array)
   def self.states_collection(f, options)
     states = collect_states(f.object.send(options[:country]))
     return f.object.send(options[:state]) if states.size == 0
     states
   end
 
-  #Return the collected States for a given Country
+  # Return the collected States for a given Country
   def self.collect_states(country)
-    CS.states(country).collect {|p| [ p[1], p[1] ] }.compact
+    CS.states(country).collect { |p| [p[1],p[1]] }.compact
   end
 
-  #Return a hash for use in the simple_form
+  # Return a hash for use in the simple_form
   def self.state_options(options)
     states = states_collection(options[:form], options[:field_names])
     options = options.merge(collection: states)
@@ -41,12 +42,12 @@ module CountryStateSelect
 end
 
 case ::Rails.version.to_s
-  when /^4/
-    require 'country_state_select/engine'
-  when /^3\.[12]/
-    require 'country_state_select/engine3'
-  when /^3\.[0]/
-    require 'country_state_select/railtie'
-  else
-    raise 'Unsupported rails version'
+when /^4/
+  require 'country_state_select/engine'
+when /^3\.[12]/
+  require 'country_state_select/engine3'
+when /^3\.[0]/
+  require 'country_state_select/railtie'
+else
+  fail 'Unsupported rails version'
 end
