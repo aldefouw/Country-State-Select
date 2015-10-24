646f903b92288f8d38227139b5c40f23533061f0
SVT390 <Adam@Mac-Pro-Adam.local>
2015-10-24 11:29:22 -0500

    Adjustments to suit the Code Climate style guide

--- app/controllers/country_state_select/cscs_controller.rb
+++ app/controllers/country_state_select/cscs_controller.rb
@@ -1,11 +1,12 @@
-#@author : Arvind Vyas
+# @author : Arvind Vyas
 module CountryStateSelect
+  # Controller renders a JSON array with the appropriate states
   class CscsController < ApplicationController
     def find_states
-      csc =   CS.states(params[:country_id])
+      csc = CS.states(params[:country_id])
 
-       respond_to do |format|
-         format.json { render :json => csc.to_a}
+      respond_to do |format|
+        format.json { render json: csc.to_a }
       end
     end
   end
