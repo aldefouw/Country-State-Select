61f9a5f2cb8a3aa1099e1e0b1b1f92fe620ffec5
SVT390 <Adam@Mac-Pro-Adam.local>
2015-10-27 19:52:35 -0600

    Try this

--- test/dummy/features/support/env.rb
+++ test/dummy/features/support/env.rb
@@ -4,89 +4,11 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 
-require 'cucumber/rails'
-require "selenium/webdriver"
+require 'capybara/cucumber'
+require 'sauce/cucumber'
 
-module SauceDriver
-  class << self
-    def sauce_endpoint
-      "http://#{ENV["SAUCE_USERNAME"]}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub"
-    end
+# To run all tests with Sauce
+Capybara.default_driver = :sauce
 
-    def caps(name)
-      caps = {
-          :platform => "Mac OS X 10.9",
-          :browserName => "Chrome",
-          :version => "31",
-          :name => name
-      }
-    end
-
-    def new_driver(name)
-      Selenium::WebDriver.for :remote, :url => sauce_endpoint, :desired_capabilities => caps(name)
-    end
-  end
-end
-
-# Capybara defaults to CSS3 selectors rather than XPath.
-# If you'd prefer to use XPath, just uncomment this line and adjust any
-# selectors in your step definitions to use the XPath syntax.
-# Capybara.default_selector = :xpath
-
-# By default, any exception happening in your Rails application will bubble up
-# to Cucumber so that your scenario will fail. This is a different from how
-# your application behaves in the production environment, where an error page will
-# be rendered instead.
-#
-# Sometimes we want to override this default behaviour and allow Rails to rescue
-# exceptions and display an error page (just like when the app is running in production).
-# Typical scenarios where you want to do this is when you test your error pages.
-# There are two ways to allow Rails to rescue exceptions:
-#
-# 1) Tag your scenario (or feature) with @allow-rescue
-#
-# 2) Set the value below to true. Beware that doing this globally is not
-# recommended as it will mask a lot of errors for you!
-#
-ActionController::Base.allow_rescue = false
-
-# Remove/comment out the lines below if your app doesn't have a database.
-# For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
-begin
-  Cucumber::Rails::Database.javascript_strategy = :truncation
-  Selenium::WebDriver.for :remote, :url => sauce_endpoint, :desired_capabilities => caps(name)
-  Capybara.javascript_driver = :remote
-
-rescue NameError
-  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
-end
-
-# You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
-# See the DatabaseCleaner documentation for details. Example:
-#
-#   Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
-#     # { :except => [:widgets] } may not do what you expect here
-#     # as Cucumber::Rails::Database.javascript_strategy overrides
-#     # this setting.
-#     DatabaseCleaner.strategy = :truncation
-#   end
-#
-#   Before('~@no-txn', '~@selenium', '~@culerity', '~@celerity', '~@javascript') do
-#     DatabaseCleaner.strategy = :transaction
-#   end
-#
-
-# Possible values are :truncation and :transaction
-# The :transaction strategy is faster, but might give you threading problems.
-# See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
-
-Before('@selenium') do
-  Capybara.current_driver = :remote
-end
-
-After('@selenium') do
-  Capybara.use_default_driver
-end
-
-
-
+# To run only JS tests against Sauce
+Capybara.javascript_driver = :sauce
---
+++ test/dummy/features/support/sauce_helper.rb
@@ -0,0 +1,14 @@
+# You should edit this file with the browsers you wish to use
+# For options, check out http://saucelabs.com/docs/platforms
+require "sauce"
+
+Sauce.config do |config|
+  config[:start_tunnel] = true
+
+
+  config[:browsers] = [
+    ["Mac OS X 10.9", "Chrome", "31"]
+  ]
+
+  config[:sauce_connect_4_executable] = "http://#{ENV["SAUCE_USERNAME"]}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub"
+end
