35c42ce48134f71c17a453c34e85840ee4388b3b
Adam De Fouw <adefouw@gmail.com>
2020-09-17 08:49:39 -0500

    Adjustments to accommodate for Rails 6

--- app/controllers/country_state_select/cscs_controller.rb
+++ app/controllers/country_state_select/cscs_controller.rb
@@ -3,9 +3,8 @@ module CountryStateSelect
   class CscsController < ApplicationController
     def find_states
       csc =   CS.states(params[:country_id])
-
-       respond_to do |format|
-         format.json { render :json => csc.to_a}
+      respond_to do |format|
+        format.json { render :json => csc.to_a}
       end
     end
   end
